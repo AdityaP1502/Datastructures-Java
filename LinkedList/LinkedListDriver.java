7b1f302655a779b0af227b9ef62be26db4a14d64
AdityaP1502 <i.m.aditya015@gmail.com>
2022-08-19 15:44:46 +0700

    LinkedList.LinkedListDriver : Update testing

--- LinkedList/LinkedListDriver.java
+++ LinkedList/LinkedListDriver.java
@@ -2,8 +2,8 @@ package LinkedList;
 
 public class LinkedListDriver {
   public static void main(String[] args) {
-    LinkedList lst1 = new LinkedList();
-    LinkedList lst2 = new LinkedList(2);
+    LinkedList<String> lst1 = new LinkedList<String>();
+    LinkedList<Integer> lst2 = new LinkedList<Integer>(2);
     
     System.out.println(lst1.isEmpty());
     lst2.insert(3, 0);
@@ -12,5 +12,7 @@ public class LinkedListDriver {
     System.out.println(lst2.pop(0));
     System.out.println(lst2.pop(1));
     lst2.print();
+    lst1.insert("Adit", 0);
+    lst1.print();
   }
 }
