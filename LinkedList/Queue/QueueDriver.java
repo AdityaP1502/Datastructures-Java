bb1f299d7dfaccd3af696d677e478af9159b2742
AdityaP1502 <i.m.aditya015@gmail.com>
2022-08-19 15:54:59 +0700

    LinkedList.QueueDriver : Update Testing

--- LinkedList/Queue/QueueDriver.java
+++ LinkedList/Queue/QueueDriver.java
@@ -2,11 +2,11 @@ package LinkedList.Queue;
 
 public class QueueDriver {
   public static void main(String[] args) {
-    Queue queue1 = new Queue(3);
+    Queue<Integer> queue1 = new Queue<Integer>(3);
     queue1.insert(1);
     queue1.insert(4);
     queue1.insert(6);
     queue1.print();
-    System.out.println(2);
+    System.out.println(queue1.pop());
   }
 }
